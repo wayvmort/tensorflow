832fd291c94bb176bd40b188253665f386a26a31
wayvmort <voldi.way@gmail.com>
2017-06-25 07:00:05 -0700

    Added more detailed comments

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2' # to suppress warnings about TensorFlow n
 import numpy as np
 import tensorflow as tf
 
+#---------------------------------------------------------------------------------------------------
+# Method 1: Use TensorFlow the old fashioned way to estimate W (aka M) and b using gradient descent
+#---------------------------------------------------------------------------------------------------
+
 # Model parameters
 W = tf.Variable([.3], tf.float32)
 b = tf.Variable([-.3], tf.float32)
+
 # Model input and output
 x = tf.placeholder(tf.float32)
 linear_model = W * x + b
 y = tf.placeholder(tf.float32)
+
 # loss
 loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+
 # optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
+
 # training data
 x_train = [1,2,3,4]
 y_train = [0,-1,-2,-3]
+
 # training loop
 init = tf.global_variables_initializer()
 sess = tf.Session()
@@ -28,6 +37,12 @@ for i in range(1000):
 # evaluate training accuracy
 curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
 print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
+#---------------------------------------------------------------------------------------------------
+# Method 2: (higher level using TFLearn): Define features and select an estimator to fit the data
+# NOTE: Most of these functions are deprecated, so there are lots of warnings. I just pulled
+# this code directly out of the TensorFlow tutorial
+#---------------------------------------------------------------------------------------------------
 
 # Declare list of features. We only have one real-valued feature. There are many
 # other types of columns that are more complicated and useful.
@@ -45,8 +60,7 @@ estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
 # of data (num_epochs) we want and how big each batch should be.
 x = np.array([1., 2., 3., 4.])
 y = np.array([0., -1., -2., -3.])
-input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x}, y, batch_size=4,
-                                              num_epochs=1000)
+input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x}, y, batch_size=4, num_epochs=1000)
 
 # We can invoke 1000 training steps by invoking the `fit` method and passing the
 # training data set.
